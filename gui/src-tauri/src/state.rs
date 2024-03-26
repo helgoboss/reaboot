489900a184ddbab31072bd37e94daf6492994528
Benjamin Klum <benjamin.klum@helgoboss.org>
2024-03-26 23:23:21 +0100

    Temporarily remove GUI from main branch

--- gui/src-tauri/src/state.rs
+++
@@ -1,8 +0,0 @@
-use crate::worker::ReabootWorkerCommand;
-use reaboot_lib::api::ReabootConfig;
-use std::sync::Mutex;
-
-pub struct ReabootAppState {
-    pub config: Mutex<ReabootConfig>,
-    pub worker_command_sender: tauri::async_runtime::Sender<ReabootWorkerCommand>,
-}
