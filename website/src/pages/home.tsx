4f18129c1d1b432f39e469c58a8014a518eef297
Benjamin Klum <benjamin.klum@helgoboss.org>
2024-04-12 17:14:36 +0200

    Embed introduction video

--- website/src/pages/home.tsx
+++ website/src/pages/home.tsx
@@ -4,7 +4,19 @@ import {NormalPage} from "../components/normal-page";
 export default function Home() {
     return (
         <NormalPage>
-            <Welcome poweredBy={false} examples={true}/>
+            <div class="flex flex-col gap-4 items-center lg:flex-row">
+                <Welcome poweredBy={false} examples={true}/>
+                <div>
+                    <h2 class="text-center text-2xl mb-4 lg:hidden">Introduction video</h2>
+                    <iframe
+                        class="w-full aspect-video"
+                        width="560" height="315"
+                        src="https://www.youtube-nocookie.com/embed/LFveUpUrHFA?si=04UBLIDqVSpfjMXD"
+                        title="YouTube video player" frameborder="0"
+                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                        referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+                </div>
+            </div>
         </NormalPage>
     );
 }
